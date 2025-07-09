b9435574629347b053d309c6a4411309aba75905
Scott Dyrness <scottdyrness@gmail.com>
2025-07-09 09:38:05 -0700

    completed classes exercise

---
+++ Classes/Car.cs
@@ -0,0 +1,8 @@
+namespace Classes;
+
+public class Car
+{
+    public string make { get; set; }
+    public string model { get; set; }
+    public int year { get; set; }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.make = "Buick";
+            carOne.model = "Encore";
+            carOne.year = 2019;
+            
+           Console.WriteLine($"This is a {carOne.make} {carOne.model}, made in the year {carOne.year}.");
         }
     }
 }
